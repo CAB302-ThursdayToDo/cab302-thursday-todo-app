6a8de7d6037f6ad026fb4745b649def4a170b928
anthonydut234 <fhorn234@gmail.com>
2026-03-26 02:58:54 +1000

    fix(project): remove JavaFX module setup for console app

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,3 @@
 module com.example.hometaskplanner {
-    requires javafx.controls;
-    requires javafx.fxml;
 
-
-    opens com.example.hometaskplanner to javafx.fxml;
-    exports com.example.hometaskplanner;
 }
